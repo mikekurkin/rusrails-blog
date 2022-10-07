b787b614e8fc249ba6e6e60e5a5a474b89aee747
Mike Kurkin <mike@kurkin.net>
2022-10-08 01:43:24 +0300

    show article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-
   # Defines the root path route ("/")
   root "articles#index"
+  
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
